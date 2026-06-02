44af03433a4854a35cd006297babd97dc60d6da0
toushobun <92347214+toushobun@users.noreply.github.com>
2026-06-02 16:17:42 +0900

    fix: add merchant edit length limits

--- src/app/(protected)/merchants/merchant-edit-form.tsx
+++ src/app/(protected)/merchants/merchant-edit-form.tsx
@@ -17,6 +17,7 @@ export function MerchantEditForm({ merchant }: MerchantEditFormProps) {
       <TextField
         defaultValue={merchant.name}
         fullWidth
+        inputProps={{ maxLength: 100 }}
         label="商家名称"
         name="name"
         required
@@ -35,6 +36,7 @@ export function MerchantEditForm({ merchant }: MerchantEditFormProps) {
       <TextField
         defaultValue={merchant.note ?? ""}
         fullWidth
+        inputProps={{ maxLength: 1000 }}
         label="备注"
         minRows={3}
         multiline
