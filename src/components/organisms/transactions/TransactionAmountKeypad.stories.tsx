72de81f06709f169f81a1218e6a0c16a46d8fe7b
toushobun <92347214+toushobun@users.noreply.github.com>
2026-06-12 13:15:51 +0900

    feat: 新增记账金额计算器输入组件

--- src/components/organisms/transactions/TransactionAmountKeypad.stories.tsx
+++ src/components/organisms/transactions/TransactionAmountKeypad.stories.tsx
@@ -1,1 +1,25 @@
-export default {};
+import type { Meta, StoryObj } from "@storybook/nextjs-vite";
+
+import { TransactionAmountKeypad } from "./TransactionAmountKeypad";
+
+const meta = {
+  title: "Organisms/Transactions/TransactionAmountKeypad",
+  component: TransactionAmountKeypad,
+  args: {
+    value: "",
+    onChange: () => {},
+    onConfirm: () => {},
+  },
+} satisfies Meta<typeof TransactionAmountKeypad>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const Jpy: Story = {
+  args: {
+    currency: "JPY",
+  },
+};
