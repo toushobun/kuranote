97bdf09f44e23a06749aebbd067ffe680f476c54
toushobun <92347214+toushobun@users.noreply.github.com>
2026-06-26 09:10:27 +0900

    feat: 添加自定义图标资产组件

---
+++ src/components/atoms/icons/kuraIconAssets.tsx
@@ -0,0 +1,105 @@
+import SvgIcon, { type SvgIconProps } from "@mui/material/SvgIcon";
+
+export function QuickRecordKuraIcon(props: SvgIconProps) {
+  return (
+    <SvgIcon viewBox="0 0 24 24" {...props}>
+      <path
+        d="M7.2 4.8h8.1c1.3 0 2.4 1.1 2.4 2.4v9.6c0 1.3-1.1 2.4-2.4 2.4H7.2c-1.3 0-2.4-1.1-2.4-2.4V7.2c0-1.3 1.1-2.4 2.4-2.4Z"
+        fill="currentColor"
+        opacity="0.18"
+      />
+      <path
+        d="M8 8.4h6.8a.8.8 0 0 1 0 1.6H8a.8.8 0 0 1 0-1.6Zm0 3.1h3.5a.8.8 0 0 1 0 1.6H8a.8.8 0 0 1 0-1.6Z"
+        fill="currentColor"
+      />
+      <path
+        d="M16.2 12.6a.8.8 0 0 1 .8.8v1.2h1.2a.8.8 0 0 1 0 1.6H17v1.2a.8.8 0 0 1-1.6 0v-1.2h-1.2a.8.8 0 0 1 0-1.6h1.2v-1.2a.8.8 0 0 1 .8-.8Z"
+        fill="currentColor"
+      />
+    </SvgIcon>
+  );
+}
+
+export function PhotoRecordKuraIcon(props: SvgIconProps) {
+  return (
+    <SvgIcon viewBox="0 0 24 24" {...props}>
+      <path
+        d="M6.6 7.2h1.9l.8-1.2c.3-.5.8-.8 1.4-.8h2.6c.6 0 1.1.3 1.4.8l.8 1.2h1.9c1.2 0 2.2 1 2.2 2.2v6.7c0 1.2-1 2.2-2.2 2.2H6.6c-1.2 0-2.2-1-2.2-2.2V9.4c0-1.2 1-2.2 2.2-2.2Z"
+        fill="currentColor"
+        opacity="0.18"
+      />
+      <path
+        d="M12 9.2a3.7 3.7 0 1 1 0 7.4 3.7 3.7 0 0 1 0-7.4Zm0 1.6a2.1 2.1 0 1 0 0 4.2 2.1 2.1 0 0 0 0-4.2Z"
+        fill="currentColor"
+      />
+      <path
+        d="M17.1 9.3a.9.9 0 1 1 0 1.8.9.9 0 0 1 0-1.8Z"
+        fill="currentColor"
+      />
+    </SvgIcon>
+  );
+}
+
+export function AccountKuraIcon(props: SvgIconProps) {
+  return (
+    <SvgIcon viewBox="0 0 24 24" {...props}>
+      <path
+        d="M4.8 8.1c0-1.2 1-2.1 2.2-2.1h10c1.2 0 2.2.9 2.2 2.1v7.8c0 1.2-1 2.1-2.2 2.1H7c-1.2 0-2.2-.9-2.2-2.1V8.1Z"
+        fill="currentColor"
+        opacity="0.18"
+      />
+      <path
+        d="M6.7 8.4h10.6a.8.8 0 0 1 0 1.6H6.7a.8.8 0 0 1 0-1.6Zm8.7 3.1h1.8c.8 0 1.4.6 1.4 1.4v1.2c0 .8-.6 1.4-1.4 1.4h-1.8a2 2 0 0 1 0-4Zm.1 1.5a.5.5 0 0 0 0 1h1.5v-1h-1.5Z"
+        fill="currentColor"
+      />
+    </SvgIcon>
+  );
+}
+
+export function CategoryKuraIcon(props: SvgIconProps) {
+  return (
+    <SvgIcon viewBox="0 0 24 24" {...props}>
+      <path
+        d="M6.8 5h3.6c1 0 1.8.8 1.8 1.8v3.6c0 1-.8 1.8-1.8 1.8H6.8c-1 0-1.8-.8-1.8-1.8V6.8c0-1 .8-1.8 1.8-1.8Zm6.8 6.8h3.6c1 0 1.8.8 1.8 1.8v3.6c0 1-.8 1.8-1.8 1.8h-3.6c-1 0-1.8-.8-1.8-1.8v-3.6c0-1 .8-1.8 1.8-1.8Z"
+        fill="currentColor"
+        opacity="0.18"
+      />
+      <path
+        d="M14.1 5.3a.8.8 0 0 1 1.1 0l3.5 3.5a.8.8 0 0 1 0 1.1l-3.5 3.5a.8.8 0 0 1-1.1 0l-3.5-3.5a.8.8 0 0 1 0-1.1l3.5-3.5Zm.6 1.7-2.3 2.3 2.3 2.3L17 9.3 14.7 7ZM7.2 13.7h3.2c.5 0 .8.3.8.8v3.2c0 .5-.3.8-.8.8H7.2a.8.8 0 0 1-.8-.8v-3.2c0-.5.3-.8.8-.8Z"
+        fill="currentColor"
+      />
+    </SvgIcon>
+  );
+}
+
+export function TagKuraIcon(props: SvgIconProps) {
+  return (
+    <SvgIcon viewBox="0 0 24 24" {...props}>
+      <path
+        d="M5.4 6.3c0-1 .8-1.8 1.8-1.8h4.5c.5 0 .9.2 1.3.5l5.7 5.7c.7.7.7 1.8 0 2.5l-5.5 5.5c-.7.7-1.8.7-2.5 0L5.9 14c-.3-.3-.5-.8-.5-1.3V6.3Z"
+        fill="currentColor"
+        opacity="0.18"
+      />
+      <path
+        d="M8.8 8.1a1.3 1.3 0 1 1 0 2.6 1.3 1.3 0 0 1 0-2.6Zm4.2 3.4 2.3 2.3a.8.8 0 0 1-1.1 1.1l-2.3-2.3a.8.8 0 0 1 1.1-1.1Z"
+        fill="currentColor"
+      />
+    </SvgIcon>
+  );
+}
+
+export function MerchantKuraIcon(props: SvgIconProps) {
+  return (
+    <SvgIcon viewBox="0 0 24 24" {...props}>
+      <path
+        d="M6.4 9.1h11.2v7.6c0 1.2-1 2.1-2.1 2.1h-7c-1.1 0-2.1-.9-2.1-2.1V9.1Z"
+        fill="currentColor"
+        opacity="0.18"
+      />
+      <path
+        d="M7.1 5.2h9.8c.7 0 1.2.4 1.5 1l1.1 2.6c.2.4 0 .9-.4 1.2-.4.4-.9.6-1.5.6-.8 0-1.5-.4-1.9-1-.4.6-1.1 1-1.9 1s-1.5-.4-1.9-1c-.4.6-1.1 1-1.9 1s-1.5-.4-1.9-1c-.4.6-1.1 1-1.9 1-.6 0-1.1-.2-1.5-.6-.4-.3-.5-.8-.4-1.2l1.1-2.6c.4-.6.9-1 1.7-1Zm2 8h5.8c.5 0 .8.3.8.8v4.1H8.3V14c0-.5.3-.8.8-.8Z"
+        fill="currentColor"
+      />
+    </SvgIcon>
+  );
+}
