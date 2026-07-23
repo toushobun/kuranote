217c61d7289891db321a2da6b3e524b9f2968381
toushobun <92347214+toushobun@users.noreply.github.com>
2026-07-23 13:17:00 +0900

    refactor: 公开 Transaction 模块契约

--- src/internal/transaction/index.ts
+++ src/internal/transaction/index.ts
@@ -1,5 +1,18 @@
 import { transactionRouter } from "internal/transaction/router";
 
+export {
+  transactionErrorCodes,
+  type EditTransactionErrorCode,
+  type NewTransactionErrorCode,
+  type TransactionErrorCode,
+  type TransactionListErrorCode,
+  type TransactionServiceErrorCode,
+  type TransactionValidationErrorCode,
+  type UpdateTransactionValidationErrorCode,
+  type VoidTransactionValidationErrorCode,
+} from "internal/transaction/errors";
+export type { TransferEditInitialValues } from "internal/transaction/service/transactionFormService";
+
 export const transactionModule = {
   basePath: "/transactions",
   name: "transaction",
