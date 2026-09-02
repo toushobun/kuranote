314f10e642a1030711f90fcbc1274089dcd19cb4
toushobun <92347214+toushobun@users.noreply.github.com>
2026-09-02 17:36:45 +0900

    style: 修正主要操作按钮 Storybook 格式

--- src/components/atoms/ui/PrimaryActionButton/PrimaryActionButton.stories.tsx
+++ src/components/atoms/ui/PrimaryActionButton/PrimaryActionButton.stories.tsx
@@ -24,10 +24,7 @@ function PrimaryActionButtonThemePreview() {
         const token = userThemeTokens[themeKey];
 
         return (
-          <ThemeProvider
-            key={themeKey}
-            theme={createDynamicMuiTheme(themeKey)}
-          >
+          <ThemeProvider key={themeKey} theme={createDynamicMuiTheme(themeKey)}>
             <Box
               style={getUserThemeCssVariables(themeKey) as CSSProperties}
               sx={{
