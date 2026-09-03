46dfc531d6b98f9738d48de2f35d80b985cb9d06
toushobun <toushobun@users.noreply.github.com>
2026-09-03 23:45:49 +0000

    deploy: dbb18492c22360a4d2b54219ba2d23819157316b

---
+++ pr-679/assets/TransactionFormHeader.stories-Dw0qbkm6.js
@@ -0,0 +1,13 @@
+import{i as e}from"./preload-helper-D2yxXLVK.js";import{n as t,t as n}from"./TransactionFormHeader-FylUNijX.js";var r,i,a,o,s;e((()=>{t(),r={title:`Organisms/Transactions/TransactionFormHeader`,component:n,args:{closeHref:`/transactions`,isSubmitDisabled:!1,ledgerName:`家庭账本`,title:`新增记账`}},i={name:`默认状态（保存可用）`},a={name:`保存按钮禁用`,args:{isSubmitDisabled:!0}},o={name:`不显示账本名`,args:{ledgerName:void 0}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  name: "默认状态（保存可用）"
+}`,...i.parameters?.docs?.source}}},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  name: "保存按钮禁用",
+  args: {
+    isSubmitDisabled: true
+  }
+}`,...a.parameters?.docs?.source}}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  name: "不显示账本名",
+  args: {
+    ledgerName: undefined
+  }
+}`,...o.parameters?.docs?.source}}},s=[`Default`,`SaveDisabled`,`NoLedgerName`]}))();export{i as Default,o as NoLedgerName,a as SaveDisabled,s as __namedExportsOrder,r as default};
