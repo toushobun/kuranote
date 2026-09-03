b019f143e1363f0c102fc90d12fe3393a372f1fc
toushobun <toushobun@users.noreply.github.com>
2026-09-03 10:42:36 +0000

    deploy: 45186b732d86c3e792c97d08bb1c37f7f9cd0fea

---
+++ pr-675/assets/EmptyState.stories-_GOLT1lN.js
@@ -0,0 +1,13 @@
+import{i as e}from"./preload-helper-D2yxXLVK.js";import{n as t,t as n}from"./EmptyState-Df3W0f46.js";var r,i,a,o;e((()=>{t(),r={component:n,title:`Molecules/UI/EmptyState`},i={name:`商家列表为空`,args:{title:`还没有商家`,description:`请先新增一个常用商家。`}},a={name:`账户列表为空`,args:{title:`还没有账户`,description:`请先新增一个账户。`}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  name: "商家列表为空",
+  args: {
+    title: "还没有商家",
+    description: "请先新增一个常用商家。"
+  }
+}`,...i.parameters?.docs?.source}}},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  name: "账户列表为空",
+  args: {
+    title: "还没有账户",
+    description: "请先新增一个账户。"
+  }
+}`,...a.parameters?.docs?.source}}},o=[`MerchantListEmpty`,`AccountListEmpty`]}))();export{a as AccountListEmpty,i as MerchantListEmpty,o as __namedExportsOrder,r as default};
