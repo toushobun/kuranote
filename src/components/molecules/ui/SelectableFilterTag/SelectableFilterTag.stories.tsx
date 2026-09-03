2140a4ed543295123815c0155f4460e4dc695f4f
toushobun <92347214+toushobun@users.noreply.github.com>
2026-09-03 13:34:04 +0900

    style: 整理筛选标签 Storybook 格式

--- src/components/molecules/ui/SelectableFilterTag/SelectableFilterTag.stories.tsx
+++ src/components/molecules/ui/SelectableFilterTag/SelectableFilterTag.stories.tsx
@@ -18,7 +18,10 @@ function SelectableFilterTagThemePreview() {
         const token = userThemeTokens[themeKey];
 
         return (
-          <ThemeProvider key={themeKey} theme={createDynamicMuiTheme(themeKey)}>
+          <ThemeProvider
+            key={themeKey}
+            theme={createDynamicMuiTheme(themeKey)}
+          >
             <Box
               style={getUserThemeCssVariables(themeKey) as CSSProperties}
               sx={{
