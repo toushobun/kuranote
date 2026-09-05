18f240ecac62a3e937bc1d8a42fa0b6554114664
toushobun <toushobun@users.noreply.github.com>
2026-09-05 12:19:14 +0000

    deploy: 944a04c3230808d919bc7124db3aa0998f3c33f6

---
+++ pr-684/assets/BottomNavButton.stories-CKISk_4X.js
@@ -0,0 +1,14 @@
+import{i as e}from"./preload-helper-D2yxXLVK.js";import{t}from"./jsx-runtime-Dwpk6tgA.js";import{t as n}from"./KuraIcon-BXikcDwJ.js";import{n as r,r as i,t as a}from"./BottomNavButton-BvHlNfvf.js";var o,s,c,l,u,d;e((()=>{o=t(),i(),r(),s={title:`Molecules/Navigation/BottomNavButton`,component:a,args:{href:`/dashboard`,label:`仪表盘`,selected:!1}},c={name:`未选中状态`},l={name:`带自定义图标`,args:{icon:(0,o.jsx)(n,{decorative:!0,name:`home`,size:`sm`})}},u={name:`选中状态`,args:{icon:(0,o.jsx)(n,{decorative:!0,name:`home`,size:`sm`}),selected:!0}},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  name: "未选中状态"
+}`,...c.parameters?.docs?.source}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  name: "带自定义图标",
+  args: {
+    icon: <KuraIcon decorative name="home" size="sm" />
+  }
+}`,...l.parameters?.docs?.source}}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  name: "选中状态",
+  args: {
+    icon: <KuraIcon decorative name="home" size="sm" />,
+    selected: true
+  }
+}`,...u.parameters?.docs?.source}}},d=[`Default`,`WithIcon`,`Selected`]}))();export{c as Default,u as Selected,l as WithIcon,d as __namedExportsOrder,s as default};
