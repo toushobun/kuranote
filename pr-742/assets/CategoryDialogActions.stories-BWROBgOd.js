6a292e4e6f0de8df0f4448cd1f783c752cdbd5d6
toushobun <toushobun@users.noreply.github.com>
2026-09-10 16:22:57 +0000

    deploy: b53a431eea78cec32010ec24a3e9b4747e2ffdc6

---
+++ pr-742/assets/CategoryDialogActions.stories-BWROBgOd.js
@@ -0,0 +1,20 @@
+import{i as e}from"./preload-helper-D2yxXLVK.js";import{t}from"./jsx-runtime-Dwpk6tgA.js";import{n,t as r}from"./Button-BvhFbL6z.js";import{n as i,t as a}from"./ArchiveRounded-BzR3Baqa.js";import{n as o,t as s}from"./CategoryDialogActions-DMKIcgRr.js";var c,l,u,d,f,p,m;e((()=>{c=t(),i(),r(),o(),l={title:`Organisms/Categories/CategoryDialogActions`,component:s,args:{onCancel:()=>{},submitLabel:`保存`}},u={name:`左右等宽操作`},d={name:`不可提交`,args:{disabled:!0}},f={name:`新增分类操作`,args:{submitLabel:`新增分类`}},p={name:`归档独立一行`,args:{children:(0,c.jsx)(n,{color:`error`,fullWidth:!0,startIcon:(0,c.jsx)(a,{}),variant:`outlined`,children:`归档该分类`})}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  name: "左右等宽操作"
+}`,...u.parameters?.docs?.source}}},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  name: "不可提交",
+  args: {
+    disabled: true
+  }
+}`,...d.parameters?.docs?.source}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  name: "新增分类操作",
+  args: {
+    submitLabel: "新增分类"
+  }
+}`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  name: "归档独立一行",
+  args: {
+    children: <Button color="error" fullWidth startIcon={<ArchiveRoundedIcon />} variant="outlined">
+        归档该分类
+      </Button>
+  }
+}`,...p.parameters?.docs?.source}}},m=[`Default`,`Disabled`,`Create`,`WithArchive`]}))();export{f as Create,u as Default,d as Disabled,p as WithArchive,m as __namedExportsOrder,l as default};
