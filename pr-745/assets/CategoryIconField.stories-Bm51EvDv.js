64503cf04a7246a6e0de2f30c24af1a54a5e8466
toushobun <toushobun@users.noreply.github.com>
2026-09-11 10:33:16 +0000

    deploy: 6d3027190f52eac157f249f9fe41990d0dfe408f

---
+++ pr-745/assets/CategoryIconField.stories-Bm51EvDv.js
@@ -0,0 +1,12 @@
+import{c as e,i as t}from"./preload-helper-D2yxXLVK.js";import{t as n}from"./react-DAMDAfNa.js";import{t as r}from"./jsx-runtime-Dwpk6tgA.js";import{n as i,t as a}from"./UserThemeProvider-DyEHxLyK.js";import{n as o,t as s}from"./CategoryIconField-kYgVXQNU.js";var c,l,u,d,f,p,m,h;t((()=>{c=r(),l=e(n()),i(),o(),{userEvent:u,within:d}=__STORYBOOK_MODULE_TEST__,f={title:`Organisms/Categories/CategoryIconField`,component:s,decorators:[e=>(0,c.jsx)(a,{storageScope:`storybook-category-icon`,children:(0,c.jsx)(e,{})})],args:{onChange:()=>{},value:`🍜`},render:function(){let[e,t]=(0,l.useState)(`🍜`);return(0,c.jsx)(s,{onChange:t,value:e})}},p={name:`Emoji 图标选择器`},m={name:`分类图标全屏分组浏览`,play:async({canvasElement:e})=>{await u.click(d(e).getByRole(`button`,{name:`选择图标`}))}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  name: "Emoji 图标选择器"
+}`,...p.parameters?.docs?.source}}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  name: "分类图标全屏分组浏览",
+  play: async ({
+    canvasElement
+  }) => {
+    await userEvent.click(within(canvasElement).getByRole("button", {
+      name: "选择图标"
+    }));
+  }
+}`,...m.parameters?.docs?.source}}},h=[`Default`,`Grouped`]}))();export{p as Default,m as Grouped,h as __namedExportsOrder,f as default};
---
+++ pr-745/assets/Menu-CWAY2teQ.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-D2yxXLVK.js";import{n as t,r as n}from"./Menu-Cjp8qL7y.js";var r=e((()=>{t(),n()}));export{r as t};
