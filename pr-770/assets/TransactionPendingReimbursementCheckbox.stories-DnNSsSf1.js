9bac725b5130c56cf9bcf8d80f3e81cdc71784c0
toushobun <toushobun@users.noreply.github.com>
2026-09-16 06:09:06 +0000

    deploy: 1e30c1eb19c0d658f2b72c957811f5cf87c3259c

---
+++ pr-770/assets/TransactionPendingReimbursementCheckbox.stories-DnNSsSf1.js
@@ -0,0 +1,11 @@
+import{c as e,i as t}from"./preload-helper-D2yxXLVK.js";import{t as n}from"./react-DAMDAfNa.js";import{t as r}from"./jsx-runtime-Dwpk6tgA.js";import{n as i,t as a}from"./TransactionPendingReimbursementCheckbox-D7IOdKfB.js";var o,s,c,l,u,d;t((()=>{o=r(),s=e(n()),i(),c={title:`Organisms/Transactions/TransactionPendingReimbursementCheckbox`,component:a,render:function(e){let[t,n]=(0,s.useState)(e.checked);return(0,o.jsx)(a,{...e,checked:t,onChange:n})}},l={args:{checked:!1,onChange(){}}},u={args:{checked:!0,onChange(){}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    checked: false,
+    onChange() {}
+  }
+}`,...l.parameters?.docs?.source}}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  args: {
+    checked: true,
+    onChange() {}
+  }
+}`,...u.parameters?.docs?.source}}},d=[`Unchecked`,`Checked`]}))();export{u as Checked,l as Unchecked,d as __namedExportsOrder,c as default};
