d22730fbedf14d638dbd77a19c3a06ceaa981ac5
toushobun <toushobun@users.noreply.github.com>
2026-09-21 18:03:18 +0000

    deploy: 5faf0563bd3beeaea1781bed395f71107d9a5e8b

---
+++ pr-794/assets/TransactionOriginalAmount.stories-BqRfr3_D.js
@@ -0,0 +1,8 @@
+import{i as e}from"./preload-helper-D2yxXLVK.js";import{n as t,t as n}from"./TransactionOriginalAmount-D5_KJirt.js";var r,i,a,o;e((()=>{t(),r={title:`Atoms/Transactions/TransactionOriginalAmount`,component:n,args:{amount:`- ¥ 500`}},i={name:`原金额`},a={name:`行内括号`,args:{parenthesized:!0}},i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  name: "原金额"
+}`,...i.parameters?.docs?.source}}},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  name: "行内括号",
+  args: {
+    parenthesized: true
+  }
+}`,...a.parameters?.docs?.source}}},o=[`Default`,`Parenthesized`]}))();export{i as Default,a as Parenthesized,o as __namedExportsOrder,r as default};
