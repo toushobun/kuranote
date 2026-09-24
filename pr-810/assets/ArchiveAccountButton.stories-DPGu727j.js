29f540d7622decc918890f9335711a794c106b1a
toushobun <toushobun@users.noreply.github.com>
2026-09-24 13:52:26 +0000

    deploy: c0786f6fbc3ea8a32b61bc0be5f21d3c1c2c455f

---
+++ pr-810/assets/ArchiveAccountButton.stories-DPGu727j.js
@@ -0,0 +1,3 @@
+import{i as e}from"./preload-helper-D2yxXLVK.js";import{t}from"./jsx-runtime-Dwpk6tgA.js";import{n,t as r}from"./UserThemeProvider-CK176g4m.js";import{n as i,t as a}from"./ConfirmDialogProvider-c01AQdhs.js";import{n as o,t as s}from"./ArchiveAccountButton-BJZU6dZa.js";var c,l,u,d;e((()=>{c=t(),i(),n(),o(),l={title:`Molecules/Accounts/ArchiveAccountButton`,component:s,decorators:[e=>(0,c.jsx)(r,{storageScope:`storybook-archive-account-button`,children:(0,c.jsx)(a,{children:(0,c.jsx)(e,{})})})]},u={name:`归档按钮`},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`{
+  name: "归档按钮"
+}`,...u.parameters?.docs?.source}}},d=[`Default`]}))();export{u as Default,d as __namedExportsOrder,l as default};
